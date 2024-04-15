5e8b4f86cddb07e4d62dcfac3cf37fa8d14017a5
unknown <apraiz.mikel@gmail.com>
2024-04-15 22:43:31 +0200

    terminable

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,26 @@
-function boxColor ({color, value}) {
+
+
+//argumento color es para comprobar que el nombre introducido en el input esta dentro del array colors
+
+function BoxColor ({color, value}) {
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+  const isValid = colors.includes(color) //si el color esta dentro del array seria valido(True)
+  
   return (
     <>
+     <div className="container">
+      {colors.map((colorName, index) =>(
+        <div key={index} className={`box ${colorName} ${colorName === value && isValid ? 'valid' : ''}`}> 
+          {colorName===value && isValid ? (
+            <p>Ies!! aim {value} kolor</p>
+          ) : (
+          <p>No! aim not a {value} kolor</p>
+          )}
+          </div>
+          
+      ))}
+      </div>
     </> 
   )
 }
-export default boxColor;  
+export default BoxColor;  
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,29 @@
+import {useState, useRef} from "react";
+import BoxColor from "./BoxColor";
 
 
 function MyForm() {
+  const[value,setValue] =useState ("");
+  const[color, setColor] =useState("");
+  const inputRef =useRef(null)
+
+  const handleInputchein = () => {
+    setColor(inputRef.current.value.toLowerCase())
+    setValue(inputRef.current.value.toLowerCase())
+  }
+
 
   return (
-    <></>
+    <>
+    <input
+      type="text"
+      placeholder="Chus a kolor"
+      value={value}
+      onChange={handleInputchein}
+      ref={inputRef}
+    />
+      <BoxColor color={color} value={value}/>
+    </>
   );
 }
 
